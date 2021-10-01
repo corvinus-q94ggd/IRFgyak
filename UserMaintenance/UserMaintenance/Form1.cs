527592dfcfb01228cbc7f78ef8b559bf4d1d4d6a
Attila Matyi <attila.matyi@stud.uni-corvinus.hu>
2021-10-01 16:37:50 +0200

    Add write to file function

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,6 +22,7 @@ namespace UserMaintenance
 
             label1.Text = Resource1.FullName;
             button1.Text = Resource1.Add;
+            button2.Text = Resource1.WriteToFIle;
 
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
@@ -35,6 +37,20 @@ namespace UserMaintenance
             };
 
             users.Add(u);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() != DialogResult.OK)
+                return;
+
+            using(var sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+                foreach (var u in users)
+                    sw.WriteLine(string.Format(
+                        "{0};{1}",
+                        u.ID,
+                        u.FullName));
         }
     }
 }

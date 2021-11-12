7928787d766ef2ab29c5a6b3fa17e994f78b7703
Attila Matyi <attila.matyi@stud.uni-corvinus.hu>
2021-11-12 19:20:51 +0100

    Ball color

--- Gyartosor/Gyartosor/Entities/Ball.cs
+++ Gyartosor/Gyartosor/Entities/Ball.cs
@@ -11,10 +11,16 @@ namespace Gyartosor.Entities
 {
     public class Ball : Toy
     {
-        protected override void DrawImage(Graphics g)
+        public SolidBrush BallColor { get; private set; }
+
+        public Ball(Color color)
         {
-            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+            BallColor = new SolidBrush(color);
         }
 
+        protected override void DrawImage(Graphics g)
+        {
+            g.FillEllipse(BallColor, 0, 0, Width, Height);
+        }
     }
 }

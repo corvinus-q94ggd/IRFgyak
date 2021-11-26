5f2f467eb3bf7754aaa44d04f953c079b4ef95dd
Attila Matyi <attila.matyi@stud.uni-corvinus.hu>
2021-11-26 19:18:15 +0100

    Password valid

--- UnitTest/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTest/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -28,5 +28,27 @@ namespace UnitTestExample.Test
             //Assert
             Assert.AreEqual(expectedResult, actualResult);
         }
+
+        [
+            Test,
+            TestCase("abcd1234", false),
+            TestCase("ABCD1234", false),
+            TestCase("abcdABCD", false),
+            TestCase("abCD12", false),
+            TestCase("abCD1234", true),
+            TestCase("abCDEF1234", true)
+        ]
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            //Act
+            var actualResult = accountController.ValidatePassword(password);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
     }
+
 }
